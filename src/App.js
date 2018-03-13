c101f7b4057a5b591c542d2fca3d5abf164fc0e4
Mariano <mariano.ramonda@altoros.com>
2018-03-13 14:58:13 -0300

    frontend working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import contract from 'truffle-contract'
+
+import AirdropContract from './contracts/Airdrop'
+import getWeb3 from './utils/getWeb3'
 
 class App extends Component {
-  render() {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      wip: false,
+      formReceipts: '',
+      formAmount: '',
+      formAddress: ''
+    }
+  }
+
+  componentWillMount () {
+    getWeb3
+      .then(results =>
+        this.setState({
+          web3: results.web3
+        })
+      ).catch(() => console.log('Error finding web3.'))
+  }
+
+  instantiateContract () {
+    const Airdrop = contract(AirdropContract)
+    Airdrop.setProvider(this.state.web3.currentProvider)
+
+    let AirdropInstance
+
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      Airdrop.deployed().then((instance) => {
+        AirdropInstance = instance
+
+        this.setState({wip: true})
+
+        return AirdropInstance.runAirdrop(
+          this.state.formReceipts.split('\n'),
+          this.state.formAmount,
+          this.state.formAddress,
+          {from: accounts[0]}
+        )
+      }).finally(() => {
+        this.setState({wip: false})
+      })
+    })
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
+  render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ethereum Airdrop</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-form">
+          <label htmlFor="receipts">Receipt addresses</label>
+          <textarea name="receipts" cols="45" rows="10" value={this.state.formReceipts} onChange={this.handleChange('formReceipts')}></textarea>
+          <label htmlFor="token_address">Token address</label>
+          <input type="text" name="address" size="45" value={this.state.formAddress} onChange={this.handleChange('formAddress')}/>
+          <label htmlFor="amount">Amount</label>
+          <input type="text" name="amount" size="45" value={this.state.formAmount} onChange={this.handleChange('formAmount')}/>
+          <button onClick={() => this.instantiateContract()}>Submit</button>
+        </div>
+        {this.state.wip && <div className="App-wip"></div>}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
